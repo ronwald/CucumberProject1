effc8dc6c03a2906976fd1d02028a02f1c04f9ce
Ronwald <ronwald.dreamer@gmail.com>
2019-10-14 18:22:54 +0800

    test

--- src/test/java/runner/testrunnerTest.java
+++ src/test/java/runner/testrunnerTest.java
@@ -24,6 +24,13 @@ public class testrunnerTest {
 	
 	public static void writeExtenReport() {
 		Reporter.loadXMLConfig(new File("config/report.xml"));
+		Reporter.setSystemInfo("User Name", System.getProperty("user.name"));
+		Reporter.setSystemInfo("Time Zone", System.getProperty("user.timezone"));
+		Reporter.setSystemInfo("Machine", "Windows 10" + "64 Bit");
+	    Reporter.setSystemInfo("Selenium", "3.7.0");
+	    Reporter.setSystemInfo("Maven", "3.5.2");
+	    Reporter.setSystemInfo("Java Version", "1.8.0_151");
+		
 		
 		
 	}
