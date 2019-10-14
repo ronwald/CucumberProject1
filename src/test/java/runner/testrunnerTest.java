523ed2d21f23ffabbf38d098d49778f95b8c8e93
Ronwald <ronwald.dreamer@gmail.com>
2019-10-14 17:49:59 +0800

    report

--- src/test/java/runner/testrunnerTest.java
+++ src/test/java/runner/testrunnerTest.java
@@ -1,6 +1,10 @@
 package runner;
 
+import java.io.File;
+
 import org.junit.runner.RunWith;
+
+import com.cucumber.listener.Reporter;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
@@ -9,11 +13,19 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(
 		features = "src/test/java/features"
 		,glue = {"seleniumgluecode"}
+		,plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"} 
+		,monochrome = true
 		
 		
 		)
 
 
 public class testrunnerTest {
+	
+	public static void writeExtenReport() {
+		Reporter.loadXMLConfig(new File("config/report.xml"));
+		
+		
+	}
 
 }
